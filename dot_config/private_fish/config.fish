6b71f30b69cb027fc91ea077e49be6fd4f980b13
The99'sPuppycat <41776348+huyhoang160593@users.noreply.github.com>
2026-09-06 13:26:45 +0700

    Add command abbreviations for exa and set universal variables for GTK and QT

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,6 +2,15 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Setup abbr with exa to replace ls
+abbr -a ls eza
+abbr -a ll eza -lbF --git
+abbr -a llm eza -lbGd --git --sort=modified
+abbr -a la eza -lbhHigUmuSa --time-style=long-iso --git --color-scale
+abbr -a lx eza -lbhHigUmuSa@ --time-style=long-iso --git --color-scale
+abbr -a lS eza -1
+abbr -a lt eza --tree --level=2
+
 # Set PATH, MANPATH, etc., for Homebrew.
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
